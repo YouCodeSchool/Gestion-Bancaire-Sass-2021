7b7874501e803419037df7f816bc251fe9b2b596
mehdilagdimi <lagdimi.mehdi@gmail.com>
2021-11-21 16:15:26 +0100

    version 0.2 : input of user accounts works as expected/fixed : had to allocate memory for every variable inside every structures

--- Bank Accounts Management.c
+++ Bank Accounts Management.c
@@ -38,18 +38,30 @@ char menuDisplay(char menu, char *choice){  //menu can take p (for principal men
 }
 
 bool createAccounts(int currentCount, int accountsNum, struct user *pStruct){
+    printf("\ncurrentCount : %d, and accountsNum to add is : %d ", currentCount, accountsNum);
     //int i = 0;
-    for(currentCount ; currentCount < accountsNum; currentCount++){
+    struct user *ptrStartOfStructArray = pStruct;
+    pStruct += currentCount;
+
+    for(currentCount ; currentCount < (currentCount + accountsNum); accountsNum--){
     printf("\nEntrer le nom d'utilisateur de compte :");
     scanf(" %s", pStruct->lName);
+    //printf("\nYou entered  : %s", pStruct->lName);
     printf("\nEntrer le prenom d'utilisateur :");
     scanf(" %s", pStruct->fName);
     printf("\nEntrer le CIN d'utilisateur :");
     scanf(" %s", pStruct->CIN);
     printf("\nEntrer le montant :");
-    scanf(" %lf", pStruct->sum); //montant as a double
+    scanf(" %lf", &pStruct->sum); //montant as a double
     system("cls");
+    //printf("\nLe montant : %lf\n", pStruct->sum);
+    pStruct++;
     }
+    /*pStruct--;
+    printf("\nfname in first element of array struct fName: %s, lName :  %s,  CIN : %s,  sum : %lf", pStruct->fName, pStruct->lName, pStruct->CIN, pStruct->sum);
+    pStruct = ptrStartOfStructArray;
+    printf("\nfname in first element of array struct fName: %s, lName :  %s,  CIN : %s,  sum : %lf", pStruct->fName,pStruct->lName, pStruct->CIN, pStruct->sum);
+    */
     return true;
 }
 void withdrawal(){}
@@ -69,16 +81,22 @@ bool loyalty(){ //fidélisation des clients
 }
 
 struct user* initiliazeUsersArray(int sizeToAllocate, struct user *userAccounts){
+    int i = 0;
     userAccounts = malloc(sizeToAllocate * sizeof(struct user)); // de don't need to explicitly cast return type of malloc unless we want to run by C++ compiler
     if(userAccounts == NULL) {printf("\nMemory not allocated for user accounts"); return NULL;}
-    userAccounts->fName = malloc((50 + 1) * sizeof(char));  //+1 is consider the terminating character of strings \0
-    if(userAccounts->fName == NULL){printf("\nMemory not allocated for fName"); return NULL;}
-    userAccounts->lName = malloc((50 + 1) * sizeof(char));
-    if(userAccounts->lName == NULL) {("\nMemory not allocated for lName"); return NULL;}
-    userAccounts->CIN = malloc((50 + 1)* sizeof(char));
-    if(userAccounts->CIN == NULL){printf("\nMemory not allocated for CIN"); return NULL;}
-    //we don't need to call malloc again for sum because it is not a pointer to something
-    return userAccounts;
+    struct user *startingPoint = userAccounts;
+    for(i; i < sizeToAllocate; i++){
+        userAccounts->fName = malloc((50 + 1) * sizeof(char));  //+1 is consider the terminating character of strings \0
+        if(userAccounts->fName == NULL){printf("\nMemory not allocated for fName"); return NULL;}
+        userAccounts->lName = malloc((50 + 1) * sizeof(char));
+        if(userAccounts->lName == NULL) {("\nMemory not allocated for lName"); return NULL;}
+        userAccounts->CIN = malloc((50 + 1)* sizeof(char));
+        if(userAccounts->CIN == NULL){printf("\nMemory not allocated for CIN"); return NULL;}
+        //we don't need to call malloc again for sum because it is not a pointer to something
+        userAccounts++;
+    }
+
+    return startingPoint;
 };
 void main(){
     char choice = '6';
@@ -88,9 +106,10 @@ void main(){
     int initialStrucSize = 20; //use i value and compare it to initialStrucSize to know if we need to realloc userAccounts struct array
                                 //i is used also to follow users order of input
     struct user *userAccounts = initiliazeUsersArray(initialStrucSize, userAccounts);
+
     do{
     if(userAccounts != NULL) menuDisplay('p', &choice);
-    printf("\n choice : %c", choice);
+    //printf("\n choice : %c", choice);
     switch (choice){
         case '1':
             if(i > initialStrucSize) userAccounts = realloc(userAccounts, i);
@@ -99,10 +118,11 @@ void main(){
             break;
         case '2':
             accountsNum = 1;
-            printf("\nCombien de compte comptes voulez-vous créer?");
-            scanf(" %d", accountsNum);
+            printf("\nCombien de compte comptes voulez-vous creer?");
+            scanf(" %d", &accountsNum);
             if(i + accountsNum > initialStrucSize) userAccounts = realloc(userAccounts, i + accountsNum);
             repeat = createAccounts(i, accountsNum, userAccounts);
+            //printf("\nAccounts num when > 1  : %d", accountsNum);
             i += accountsNum;
             break;
         case '3':
